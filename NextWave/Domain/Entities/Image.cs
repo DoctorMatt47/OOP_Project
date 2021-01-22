2bbedc2f7d72f8d2524d3c2b8172a330310f5542
DoctorMatt <DoctorMatt47@gmail.com>
2021-01-22 20:50:27 +0200

    Added entities for database

---
+++ NextWave/Domain/Entities/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextWave.Domain.Entities
+{
+    public class Image
+    {
+        public string Full { get; set; }
+        public string Sprite { get; set; }
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string W { get; set; }
+        public string H { get; set; }
+    }
+}
